142ceff324a77f254824e8a30a12e59d460e437b
Marco Giusti <marco.giusti@dashboard.lu>
2018-01-18 22:10:21 +0100

    Ignore dbapi20 warnings

--- test/test_dbapi20_adsdb3.py
+++ test/test_dbapi20_adsdb3.py
@@ -1,5 +1,7 @@
 # Copyright (c) 2018 Marco Giusti
 
+import warnings
+
 import dbapi20
 import adsdb3
 from adsdb3_test_utils import ConnectMixin
@@ -9,6 +11,27 @@ class TestADS_DBAPI20(ConnectMixin, dbapi20.DatabaseAPI20Test):
 
     driver = adsdb3
     lower_func = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls.catcher = warnings.catch_warnings()
+        cls.catcher.__enter__()
+        warnings.filterwarnings(
+            'ignore',
+            'Implicit cleanup Statement',
+            ResourceWarning,
+            'adsdb3'
+        )
+        warnings.filterwarnings(
+            'ignore',
+            'Please use assertTrue instead',
+            DeprecationWarning,
+            'dbapi20'
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.catcher.__exit__(None, None, None)
 
     def test_setoutputsize(self):
         # not implemented
