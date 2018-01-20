d81169ce4ab4d37a4ce57f7400d8635dc021b6a0
Marco Giusti <marco.giusti@dashboard.lu>
2018-01-20 17:02:54 +0100

    Simplify tests

--- test/test_ace.py
+++ test/test_ace.py
@@ -14,55 +14,39 @@ class TestACE(unittest.TestCase):
             lib.ads_fini()
 
     def test_new_connection(self):
-        handler = None
-        try:
-            handler = lib.ads_new_connection()
-            self.assertIsNotNone(handler)
-        finally:
-            if handler:
-                lib.ads_free_connection(handler)
+        handler = lib.ads_new_connection()
+        if handler:
+            self.addCleanup(lib.ads_free_connection, handler)
+        self.assertIsNotNone(handler)
 
     def test_free_connection_null(self):
         lib.ads_free_connection(ffi.NULL)
 
     def test_connect(self):
-        handler = None
-        rc = 0
-        try:
-            handler = lib.ads_new_connection()
-            rc = lib.ads_connect(handler, b'')
-            try:
-                self.assertEqual(rc, 0)
-            finally:
-                if rc:
-                    lib.ads_disconnect(handler)
-        finally:
-            if handler:
-                lib.ads_free_connection(handler)
+        handler = lib.ads_new_connection()
+        if handler:
+            self.addCleanup(lib.ads_free_connection, handler)
+        rc = lib.ads_connect(handler, b'')
+        self.addCleanup(lib.ads_disconnect, handler)
+        self.assertEqual(rc, 0)
 
     def test_disconnect(self):
-        handler = None
-        try:
-            handler = lib.ads_new_connection()
-            self.assertIsNotNone(handler)
-            self.assertEqual(lib.ads_disconnect(handler), 1)
-        finally:
-            if handler:
-                lib.ads_free_connection(handler)
+        handler = lib.ads_new_connection()
+        if handler:
+            self.addCleanup(lib.ads_free_connection, handler)
+        self.assertIsNotNone(handler)
+        self.assertEqual(lib.ads_disconnect(handler), 1)
 
     def test_disconnect_null(self):
         self.assertEqual(lib.ads_disconnect(ffi.NULL), 0)
 
     def test_disconnect_twice(self):
-        handler = None
-        try:
-            handler = lib.ads_new_connection()
-            self.assertIsNotNone(handler)
-            self.assertEqual(lib.ads_disconnect(handler), 1)
-            self.assertEqual(lib.ads_disconnect(handler), 1)
-        finally:
-            if handler:
-                lib.ads_free_connection(handler)
+        handler = lib.ads_new_connection()
+        if handler:
+            self.addCleanup(lib.ads_free_connection, handler)
+        self.assertIsNotNone(handler)
+        self.assertEqual(lib.ads_disconnect(handler), 1)
+        self.assertEqual(lib.ads_disconnect(handler), 1)
 
     def test_commit_null(self):
         self.assertEqual(lib.ads_commit(ffi.NULL), 0)
